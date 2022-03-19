08e92c0edee923311b2f0673639edde3ef3e76a0
Pascual <pterlizzi@gmail.com>
2022-03-19 14:38:23 +0000

    Configuring Terraform backend for Azure Resource Group

--- lab-2-kubernetes-resources/terraform/aks-cluster/backend.tf
+++ lab-2-kubernetes-resources/terraform/aks-cluster/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "github-actions-tfstate"
-    storage_account_name = " pterlizzigithubactionstfstatept"
+    storage_account_name = "githubactionstfstatept"
     container_name       = "tfstatedevops"
     key                  = "lab-2-aks-cluster.tfstate"
   }
